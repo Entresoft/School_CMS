43d78b4a10b01479aab27c789691996fb9f1964a
samsam2310 <samsam2310@gmail.com>
2015-05-06 23:14:01 +0800

    alias React Bootstrap to RB

--- schoolcms/static/js/schoolcms.js
+++ schoolcms/static/js/schoolcms.js
@@ -1,56 +1,34 @@
 /** @jsx React.DOM */
 
-var Alert = ReactBootstrap.Alert;
-var Navbar = ReactBootstrap.Navbar;
-var Nav = ReactBootstrap.Nav;
-var NavItem = ReactBootstrap.NavItem;
-var DropdownButton = ReactBootstrap.DropdownButton;
-var MenuItem = ReactBootstrap.MenuItem
+const RB = ReactBootstrap;
+var Alert = RB.Alert;
 
 
-var NavbarInstance = React.createClass({
+const NavbarInstance = React.createClass({
   userSign: function(){
     if( this.props.current_user ){
       return (
-        <DropdownButton eventKey={3} title={ this.props.current_user.name }>
-          <MenuItem eventKey='1'>Action</MenuItem>
-          <MenuItem eventKey='2'>Another action</MenuItem>
-          <MenuItem eventKey='3'>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey='4' href="/logout">Logout</MenuItem>
-        </DropdownButton>
+        <RB.DropdownButton eventKey={3} title={ this.props.current_user.name }>
+          <RB.MenuItem eventKey='1'>Action</RB.MenuItem>
+          <RB.MenuItem eventKey='2'>Another action</RB.MenuItem>
+          <RB.MenuItem eventKey='3'>Something else here</RB.MenuItem>
+          <RB.MenuItem divider />
+          <RB.MenuItem eventKey='4' href="/logout">Logout</RB.MenuItem>
+        </RB.DropdownButton>
       )
     }else{
-      return <NavItem eventKey={3} href='/login'>Login</NavItem>
+      return <RB.NavItem eventKey={3} href='/login'>Login</RB.NavItem>
     }
   },
   render: function(){
     return (
-      <Navbar brand={<a href="/">School Cms</a>} inverse toggleNavKey={0}>
-        <Nav right eventKey={0}> {/* This is the eventKey referenced */}
-          <NavItem eventKey={1} href='#'>Link</NavItem>
-          <NavItem eventKey={2} href='#'>Link</NavItem>
+      <RB.Navbar brand={<a href="/">School Cms</a>} inverse toggleNavKey={0}>
+        <RB.Nav right eventKey={0}> {/* This is the eventKey referenced */}
+          <RB.NavItem eventKey={1} href='#'>Link</RB.NavItem>
+          <RB.NavItem eventKey={2} href='#'>Link</RB.NavItem>
           { this.userSign() }
-        </Nav>
-      </Navbar>
+        </RB.Nav>
+      </RB.Navbar>
     );
   },
 });
-
-// const navbarInstance = (
-//       <Navbar brand='React-Bootstrap' inverse toggleNavKey={0}>
-//         <Nav right eventKey={0}> {/* This is the eventKey referenced */}
-//           <NavItem eventKey={1} href='#'>Link</NavItem>
-//           <NavItem eventKey={2} href='#'>Link</NavItem>
-//           <DropdownButton eventKey={3} title='Dropdown'>
-//             <MenuItem eventKey='1'>Action</MenuItem>
-//             <MenuItem eventKey='2'>Another action</MenuItem>
-//             <MenuItem eventKey='3'>Something else here</MenuItem>
-//             <MenuItem divider />
-//             <MenuItem eventKey='4'>Separated link</MenuItem>
-//           </DropdownButton>
-//         </Nav>
-//       </Navbar>
-//     );
-
-// React.render(navbarInstance, document.getElementById('navbar'));
